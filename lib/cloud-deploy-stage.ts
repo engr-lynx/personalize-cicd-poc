082e1e6f6dbd6f3fd9a674bf00ddffec3e2070d6
Leon Valencia <techie@lynx.rocks>
2021-06-27 21:08:46 +0800

    single cache bucket

--- lib/cloud-deploy-stage.ts
+++ lib/cloud-deploy-stage.ts
@@ -1,13 +1,17 @@
 import { Construct, Stage, StageProps } from '@aws-cdk/core';
 import { EmptyStack } from './empty-stack';
 
+interface CloudDeployProps extends StageProps {
+  cacheBucketArn?: string,
+}
+
 /**
  * Deployable unit of entire architecture
  */
 export class CloudDeployStage extends Stage {
 
-  constructor(scope: Construct, id: string, props?: StageProps) {
-    super(scope, id, props);
+  constructor(scope: Construct, id: string, cloudDeployProps?: CloudDeployProps) {
+    super(scope, id, cloudDeployProps);
     new EmptyStack(this, 'Sample');
   }
 
